7a00d550c043e612e5c946d280d6dcf4eaa1d590
Julian Lopez <jlopezsa@gmail.com>
2023-03-19 22:11:55 -0500

    Begin project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './screens/landing/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello ATBionano</h2>
+        <LandingPage />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/screens/landing/LandingPage.jsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>This is the ATBionano Landing Page</h1>
+        </div>
+    )
+}
+
+export default LandingPage;
