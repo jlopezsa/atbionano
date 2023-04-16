0d0f9568407f8477cdb884022fab7f098e9813d0
Julian Lopez <jlopezsa@gmail.com>
2023-04-16 13:22:12 -0500

    ADD: Adjust styles to responsive design

--- src/components/cards/callsProcesses/CallsProcesses.jsx
+++ src/components/cards/callsProcesses/CallsProcesses.jsx
@@ -5,7 +5,7 @@ const CallsProcesses = ({props}) => {
     return(
         <div class={`relative -z-10 w-80 h-[400px] mt-6 bg-[${Color.greenDark}] rounded-xl drop-shadow-md hover:drop-shadow-xl`}>
             <div class={`w-80 h-[200px] bg-[${Color.greenPastel}] rounded-t-xl grid justify-items-center items-center`}>
-                <img class="w-80 h-auto rounded-t-xl" src={props.imagePath} alt="logo" />
+                <img class="w-80 h-auto" src={props.imagePath} alt="logo" />
             </div>
             <div class="h-[30px]text-xl font-bold text-white pl-[10px]">
                 {props.title}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import {Color} from "../../configs/colors"
 const Header = () => {
     return(
         <div class={`relative sticky top-0 h-[80px] bg-[${Color.greenDark}] md:flex md:flex-nowrap md:justify-between md:items-center z-1`}>
-            <div class="max-w md:block inset-y-0 right-0  w-[400px] h-[80px]">
+            <div class="max-w md:block inset-y-0 right-0 h-[80px]">
                 <a href={`#${ROUTE_HOME_TOP}`}>
                     <img class="float-none pl-[50px] md:float-right h-[80px] w-auto h-[80px] w-auto" src={logoAtbionano} alt="logo" />
                 </a>
