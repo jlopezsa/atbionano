d3878375bcb1989e3be4ccbba94c3c58c1b8bfc5
Julian Lopez <jlopezsa@gmail.com>
2023-08-04 22:03:03 -0500

    ADD: Carousel with button to navigate another section

--- src/components/carouselMain/CarouselMain.jsx
+++ src/components/carouselMain/CarouselMain.jsx
@@ -1,24 +1,37 @@
  
 import { Carousel } from '@material-tailwind/react';
+import { ButtonLight } from './../buttons/ButtonLight/ButtonLight';
 
 const CarouselMain = () => {
   return (
-    <Carousel className="rounded-xl h-[50%] w-[80%] absolute">
-      <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-        alt="Beautiful landscape"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="City skyline at night"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="Colorful artwork"
-        className="h-full w-full object-cover"
-      />
+    <Carousel className="rounded-sm h-[90vh] w-[100%] absolute z-0 mt-[80px]" loop={true}>
+      <div className="m-[0] p-[0] h-[90vh]">
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="Beautiful landscape"
+          className="h-full w-full object-cover"
+        />
+        <div class="absolute bottom-[200px] ml-[20px]  sm:ml-[100px] z-10">
+          <ButtonLight message={"Conoce más sobre nuestra metodología"}/>
+        </div>
+      </div>
+      <div className="m-[0] p-[0] h-[90vh]">
+        <img
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="City skyline at night"
+          className="h-full w-full object-cover"
+        />
+        <div class="absolute bottom-[200px] ml-[20px]  sm:ml-[100px] z-10">
+          <ButtonLight message={"Conoce nuestros productos"}/>
+        </div>
+      </div>
+      <div className="m-[0] p-[0] h-[90vh]">
+        <img
+          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          alt="Colorful artwork"
+          className="h-full w-full object-cover"
+        />
+      </div>
     </Carousel>
   );
 }
--- src/components/slides/Slides.jsx
+++ src/components/slides/Slides.jsx
@@ -1,16 +1,19 @@
 import backgroundImage from "../../images/main-background-2.jpg";
+import { ButtonLight } from "../buttons/ButtonLight/ButtonLight";
 import CarouselMain from "../carouselMain/CarouselMain";
 
 const Slides = () => {
     return(
         <div>
-            <div class="bg-fixed h-[100vh] " style={{backgroundImage: `url(${backgroundImage})`} }>
-                <div class="pl-[20px] pb-[50px] pt-[100px] text-3xl text-white md:pl-[100px] md:pt-[150px]">
+            <div class="relative bg-fixed h-[100vh] ">
+                {/* <div class="bg-fixed h-[100vh] " style={{backgroundImage: `url(${backgroundImage})`} }> */}
+                <div class="pl-[20px] pb-[10px] pt-[200px] text-3xl text-white absolute z-10 md:pl-[100px] md:pt-[200px]">
                     <p>Innovación y desarrollo de productos de base Bionanotecnológica.</p>
                 </div>
                 <div class="flex justify-center">
                     <CarouselMain />
                 </div>
+                
             </div>
         </div>
     )
