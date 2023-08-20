66802d47ad80dc0a0173a2b9238728d288839409
LopezPin <lopezpin@MacBook-Air-de-LopezPin.local>
2023-08-19 23:51:01 -0500

    ADD: Responsive design into carousel

--- src/components/carouselMain/CarouselSpecificContent.jsx
+++ src/components/carouselMain/CarouselSpecificContent.jsx
@@ -1,32 +1,31 @@
 import { ButtonLight } from "./../buttons/ButtonLight/ButtonLight";
 const CarouselSpecificContent = ({ title, topMessage, bottomMessage, image, messageButton }) => {
   return (
-    // <div class="absolute flex flex-col ml-[15%] w-[80%] top-[30%] text-2xl text-white
-    // sm:top-[40%] sm:start-[100px] sm:text-5xl sm:flex sm:flex-row sm:justify-between sm:ml-[0]">
-    <div
-      class="absolute max-w-screen-2xl flex flex-col flex-wrap items-center  ml-[15%] w-[80%] top-[20%] text-2xl text-white 
-      sm:top-[25%] sm:text-5xl sm:flex sm:flex-row sm:justify-between sm:ml-[0]"
+      <div
+      class="absolute max-w-screen-2xl flex flex-wrap items-center w-[80%] top-[15%] text-white"
     >
-      <div class="start-[10px] h-[100px] text-2xl text-white z-10 sm:start-[100px] sm:text-3xl">
+      <div class="text-2xl z-10 w-full h-28">
         <strong>
           {title}
         </strong>
       </div>
 
-      <div class="flex flex-col flex-nowrap">
+      <div class="text-3xl flex flex-row flex-wrap items-center justify-center sm:justify-between">
         <div>
-          <strong>{topMessage}</strong>
+          <div>
+            <strong>{topMessage}</strong>
+          </div>
+          <div class="text-2xl text-white">
+            <p>{bottomMessage}</p>
+          </div>
         </div>
-        <div class="text-2xl text-white">
-          <p>{bottomMessage}</p>
+        <div class="w-[40%] min-w-[300px] min-h-[200px] flex items-center">
+          <img src={image} alt="atbionano patente" class="rounded-xl" />
         </div>
       </div>
 
-      <div class="w-[280px] h-[auto] md:w-[600px] mt-[20px] md:h-[auto]">
-        <img src={image} alt="atbionano patente" class="rounded-xl" />
-      </div>
 
-      <div class="bottom-[10%] start-[20px]  sm:start-[100px] z-10">
+      <div class="z-10 w-full h-16 flex items-center justify-center sm:justify-start">
         <ButtonLight
           message={messageButton}
         />
