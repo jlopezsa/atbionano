18031683dd16a42c75c6211268de9da6cdde6a0b
LopezPin <lopezpin@MacBook-Air-de-LopezPin.local>
2023-08-21 10:47:04 -0500

    ADD: NavBar with atbionano sections

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
 import logoAtbionano from "../../images/logo_letraBlanca.png";
 import Menu from "./../menu/Menu";
 import { ROUTE_HOME } from "../../routes/routes";
+import NavBarWithSections from "../navBar/NavBarWithSections";
 
 const Header = () => {
   return (
     <div
       class={`fixed top-0 z-50 w-full h-[80px] bg-primary md:flex md:flex-nowrap md:justify-between md:items-center fixed`}
     >
-      <div class="max-w md:block inset-y-0 right-0 h-[80px] flex items-center md:flex md:items-center">
+      {/* <div class="max-w md:block inset-y-0 right-0 h-[80px] flex items-center md:flex md:items-center">
         <a href={`${ROUTE_HOME}`}>
           <img
             class="float-none pl-[50px] md:float-right h-[65px] w-auto h-[65px] w-auto"
@@ -15,8 +16,8 @@ const Header = () => {
             alt="logo"
           />
         </a>
-      </div>
-      <Menu />
+      </div> */}
+      <NavBarWithSections />
     </div>
   );
 };
