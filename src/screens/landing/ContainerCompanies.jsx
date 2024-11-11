ed8338eadef300766639f6c8fa7e4c2b533ea331
LopezPin <lopezpin@MacBook-Air-de-LopezPin.local>
2024-11-11 16:27:54 -0500

    ADD: Infinit scroll to companies list

--- src/screens/landing/ContainerCompanies.jsx
+++ src/screens/landing/ContainerCompanies.jsx
@@ -1,21 +1,39 @@
-import { Typography } from "@material-tailwind/react";
-import Companies from "components/cards/companies/Companies";
-import { CompaniesMessages } from "configs/companiesMessages";
-import { ROUTE_SECTION_PARTNERS } from "routes/routes";
+import { Typography } from "@material-tailwind/react"
+import Companies from "components/cards/companies/Companies"
+import { CompaniesMessages } from "configs/companiesMessages"
+import { ROUTE_SECTION_PARTNERS } from "routes/routes"
+
+// Help to inifinite scroll
+// https://cruip.com/create-an-infinite-horizontal-scroll-animation-with-tailwind-css/
 
 const ContainerCompanies = () => {
+  const allCompanies = [
+    CompaniesMessages.company1,
+    CompaniesMessages.company2,
+    CompaniesMessages.company3,
+  ]
   return (
     <div id={ROUTE_SECTION_PARTNERS}>
-      <Typography variant="h3" className="text-center pt-[80px]">
+      <Typography variant="h3" className="pt-[80px] text-center">
         Empresas aliadas
       </Typography>
-      <div class="flex flex-row justify-around flex-wrap">
-        <Companies props={CompaniesMessages.company1} />
-        <Companies props={CompaniesMessages.company2} />
-        <Companies props={CompaniesMessages.company3} />
+      <div className="inline-flex w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+        <ul class="animate-infinite-scroll flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8">
+          {allCompanies.map((company) => (
+            <Companies props={company} />
+          ))}
+        </ul>
+        <ul
+          class="animate-infinite-scroll flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8"
+          aria-hidden="true"
+        >
+          {allCompanies.map((company) => (
+            <Companies props={company} />
+          ))}
+        </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ContainerCompanies;
+export default ContainerCompanies
